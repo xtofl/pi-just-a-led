8c50a2d9819621c0e190281e2b60d5226479825d
xtofl <kristoffel.pirard@gmail.com>
2015-08-08 12:47:01 +0000

    [fix] cleanup GPIO after use

--- on.py
+++ on.py
@@ -6,10 +6,16 @@ def main():
     pin = int(sys.argv[1])
     print("light on for pin {}".format(pin))
 
-    from RPi import GPIO
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, True)
+    try:
+        from RPi import GPIO
+        GPIO.setmode(GPIO.BOARD)
+        try:
+            GPIO.setup(pin, GPIO.OUT)
+        except RuntimeError as e:
+            print("error setting pin {} to OUT: {}".format(pin, e))
+        GPIO.output(pin, True)
+    finally:
+        GPIO.cleanup()
     
 if __name__ == "__main__":
     main()
