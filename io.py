d56817bd2f396291368f26e9de9c1fa487989d18
xtofl <kristoffel.pirard@gmail.com>
2015-08-17 11:07:39 +0000

    [refac] extract io class
    This class can 'deliver' pins, make sure pins aren't
    assigned twice, can be mocked, ...

--- io.py
+++ io.py
@@ -1,35 +1,57 @@
 from RPi import GPIO
 
+def init_gpio():
+    GPIO.setmode(GPIO.BOARD)
+    return GPIO
+
+class IO:
+    def __init__(self, gpio):
+        self.gpio = gpio
+
+    def start(self):
+        self.gpio.setmode(self.gpio.BOARD)
+
+    def inpin(self, pin):
+        return Input(pin)
+
+    def outpin(self, pin, initial=False):
+        return Output(pin, initial)
+
+    def stop(self):
+        return self.gpio.cleanup()
+
 class Output:
-    def __init__(self, pin, initial=False):
+    def __init__(self, pin, initial=False, io=GPIO):
+        self.io = io
         self.pin = pin
         self.initial = initial
 
     def start(self):
-        GPIO.setup(self.pin, GPIO.OUT)
-        GPIO.output(self.pin, self.initial)
+        self.io.setup(self.pin, self.io.OUT)
+        self.io.output(self.pin, self.initial)
 
     def on(self):
-        GPIO.output(self.pin, True)
+        self.io.output(self.pin, True)
 
     def off(self):
-        GPIO.output(self.pin, False)
+        self.io.output(self.pin, False)
 
     def set(self, value):
-        GPIO.output(self.pin, value)
+        self.io.output(self.pin, value)
 
 class Input:
-    def __init__(self, pin):
+    def __init__(self, pin, io=GPIO):
+        self.io = io
         self.pin = pin
-        self.mode = GPIO.IN
+        self.mode = self.io.IN
         self.callbacks = []
 
     def start(self):
-        GPIO.setup(self.pin, GPIO.IN)
-        GPIO.add_event_detect(self.pin, GPIO.BOTH, callback=self.__event_detect)
+        self.io.setup(self.pin, self.io.IN)
+        self.io.add_event_detect(self.pin, self.io.BOTH, callback=self.__event_detect)
 
     def on(self):
-        return GPIO.input(self.pin)
+        return self.io.input(self.pin)
 
     def callback(self, what):
         self.callbacks.append(what)
