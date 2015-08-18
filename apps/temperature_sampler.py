af915269c75955179b27b268a0646c66b883e704
xtofl <kristoffel.pirard@gmail.com>
2015-08-18 17:50:18 +0000

    [impl] calculate temperature from voltage over thermistor

--- apps/temperature_sampler.py
+++ apps/temperature_sampler.py
@@ -5,14 +5,17 @@ from time import sleep
 
 #FIXME: add 'justaled' to the PYTHONPATH
 sys.path.append(os.path.dirname(os.path.dirname(sys.argv[0])))
+from justaled import epoxy_thermistor
 
 def resistorvalue(inputvoltage, voltage, otherresistor):
    return otherresistor * (voltage / (inputvoltage - voltage))
 
 
 def sample(mcp):
-    value = mcp.read(channel=0)
-    return value
+    voltage = mcp.read(channel=0)
+    resistance = 10.0 / ( 1.0 - voltage / mcp.vref )
+    temperature = epoxy_thermistor.temperature(resistance)
+    return "{}V, {} kOhm, {} C".format(voltage, resistance, temperature)
 
 def publish(what):
     print(what)
@@ -24,7 +27,7 @@ def main():
     from justaled.io import IO
     from time import sleep
     io = IO(GPIO)
-    mcp = Mcp3008(io, reference_voltage=5.0)
+    mcp = Mcp3008(io, reference_voltage=3.3)
     io.start()
     mcp.start()
     while True:
