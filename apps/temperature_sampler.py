dfd9139b39b653fb9ba88f17d75b7662b33d8957
xtofl <kristoffel.pirard@gmail.com>
2015-08-18 14:06:53 +0000

    [hack] add '../..' to PYTHONPATH

--- apps/temperature_sampler.py
+++ apps/temperature_sampler.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 import sys
+import os
 from time import sleep
+
+#FIXME: add 'justaled' to the PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(sys.argv[0])))
 
 def resistorvalue(inputvoltage, voltage, otherresistor):
    return otherresistor * (voltage / (inputvoltage - voltage))
@@ -14,10 +18,12 @@ def publish(what):
     print(what)
 
 def main():
-    interval = sys.argv[1]
-    from mcp3008 import Mcp3008
-    from io import IO
-    io = IO()
+    interval = float(sys.argv[1])
+    from RPi import GPIO
+    from justaled.mcp3008 import Mcp3008
+    from justaled.io import IO
+    from time import sleep
+    io = IO(GPIO)
     mcp = Mcp3008(io, reference_voltage=5.0)
     io.start()
     mcp.start()
