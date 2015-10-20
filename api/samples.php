b7ab02719cd51e0f19617bd94ffe469da1da215f
saraxtofl <kristoffel.pirard@gmail.com>
2015-10-20 15:08:08 +0000

    [impl] add filter function /api/samples.php?filter=all|week|day

--- api/samples.php
+++ api/samples.php
@@ -1,12 +1,22 @@
 <?php
 
-$format = $_GET["format"];
-
 function samples_text() {
 	return rtrim(file_get_contents("../temperatures.jsonsamples"));
 }
 
-function samples_json() {
+function all($ample) { return true; }
+
+function last_day($sample) {
+	$lastweek = strtotime("-1 day");
+	return strtotime($sample->time) > $lastweek;
+}
+
+function last_week($sample) {
+	$lastweek = strtotime("-1 week");
+	return strtotime($sample->time) > $lastweek;
+}
+
+function samples_json($filter) {
 	$input = rtrim(samples_text());
 	$objects = [];
 	$errors = [];
@@ -19,9 +29,7 @@ function samples_json() {
 			$objects[] = $object;
 		}
 	}
-	$data = $objects;
-	if(!$data) { print("json error: ".json_last_error()); }
-	return $objects;
+	return array_filter($objects, $filter);
 }
 
 function sample_row($time, $value, $temperature) {
@@ -32,11 +40,10 @@ function sample_row($time, $value, $temperature) {
 		sprintf("%20s", $temperature),
 		""
 	]);
-	
 }
 
-function samples_table() {
-	$samples = samples_json();
+function samples_table($filter) {
+	$samples = samples_json($filter);
 	$rows = [sample_row("time", "value (kOhm)", "temperature (C)"), ""];
 	foreach($samples as $sample) {
 		$rows[] = sample_row(
@@ -48,14 +55,27 @@ function samples_table() {
 	return implode("\n", $rows);
 }
 
+function filter() {
+	switch ($_GET["filter"]) {
+		case "all": return "all";
+		case "week": return "last_week";
+		case "day": return "last_day";
+		default: return "last_week";
+	}
+}
+$filter = filter();
+
+$format = $_GET["format"];
+
 switch ($format) {
 	case "table":
 		header("Content-Type: text/plain");
-		echo samples_table();
+		echo samples_table($filter);
 		break;
+	case "json":
 	default:
 		header("Content-Type: application/json");
-		echo samples_text();
+		echo samples_text($filter);
 		break;
 }
 ?>
